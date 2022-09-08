3cba5db0aab858f4af7cd21ddb426adef920cd8d
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-08 12:27:26 +0200

    Esercitazione sulle Struct

--- Library/Program.cs
+++ Library/Program.cs
@@ -4,20 +4,14 @@
         This class represents a User.
     */
     public class User {
-        private string id;
-        private string name;
-        private string lastName;
-        private int yearOfSubscription;
-        
-        public User (string id, string name, string lastName, int yearOfSubscription) {
-            this.id = id;
-            this.name = name;
-            this.lastName = lastName;
-            this.yearOfSubscription = yearOfSubscription;
-        }
-
-        public string Denomination() {
-            return $"{this.name} {this.lastName}";
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int YearOfSubscription { get; set; }
+        public string Denomination {
+            get {
+                return $"{Name} {LastName}";
+            }
         }
     }
 
@@ -25,37 +19,32 @@
         This class represents a Book.
     */
     public class Book {
-        private string id;
-        private string title;
-        private string author;
-        private User user;
-
-        public Book (string id, string title, string author) {
-            this.id = id;
-            this.title = title;
-            this.author = author;
-            this.user = null;
-        }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        private User user = null;
 
         /* Method to return a custom description of a book */
-        public string Description() {
-            return String.Format("'{0}' by {1}", this.title, this.author);
+        public string Description {
+            get {
+                return String.Format("'{0}' by {1}", Title, Author);
+            }
         }
 
         /* Method use to loan a book to an user */
         public void LoanTo(User user) {
             if (this.user != null) {
-                Console.WriteLine($"The book '{this.title}' is already loaned by {this.user.Denomination()}");
+                Console.WriteLine($"The book '{Title}' is already loaned by {this.user.Denomination}");
                 return;
             }
 
             this.user = user;
-            Console.WriteLine($"The book '{this.title}' has been loaned to {this.user.Denomination()}");
+            Console.WriteLine($"The book '{Title}' has been loaned to {this.user.Denomination}");
         }
 
         /* Method use to return back the book */
         public void ReturnBack() {
-            Console.WriteLine($"The user {this.user.Denomination()} returned back the book {this.Description()}");
+            Console.WriteLine($"The user {this.user.Denomination} returned back the book {this.Description}");
             this.user = null;
         }
     }
@@ -65,14 +54,14 @@
     */
     public class TheLibrary {
         public static void Main(string[] args) {
-            User firstUser = new User("0001", "Ernesto", "Acosta", 2022);
-            Console.WriteLine($"User {firstUser.Denomination()} created.");
+            User firstUser = new User { Id = "0001", Name = "Ernesto", LastName = "Acosta", YearOfSubscription = 2022 };
+            Console.WriteLine($"User {firstUser.Denomination} created.");
 
-            User secondUser = new User("0002", "Paola", "Damiani", 2021);
-            Console.WriteLine($"User {secondUser.Denomination()} created.");
+            User secondUser = new User { Id = "0002", Name = "Paola", LastName = "Damiani", YearOfSubscription = 2021 };
+            Console.WriteLine($"User {secondUser.Denomination} created.");
 
-            Book book = new Book("L001", "Cent'anni di solitudine", "Gabriel Garcia Marquez");
-            Console.WriteLine($"Book {book.Description()} created.");
+            Book book = new Book{ Id = "L001", Title = "Cent'anni di solitudine", Author = "Gabriel Garcia Marquez" };
+            Console.WriteLine($"Book {book.Description} created.");
 
             // To loan the book to the first user
             book.LoanTo(firstUser);
---
+++ Strutture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Strutture
+{
+    public class MyClass {
+        public string myString;
+        public int myInteger;
+    }
+
+    public struct MyStruct {
+        public string myString;
+        public int myInteger;
+    }
+    public class Program {
+        static void Main(string[] args) {
+            MyClass myClass = new MyClass();
+            myClass.myInteger = 1;
+            myClass.myString = "Ernesto";
+
+            MyStruct myStruct = new MyStruct();
+            myStruct.myInteger = 2;
+            myStruct.myString = "Paola";
+
+            Console.WriteLine($"Prima myClass.myInteger = {myClass.myInteger}, myClass.myString = {myClass.myString}");
+            Console.WriteLine($"Prima myStruct.myInteger = {myStruct.myInteger}, myStruct.myString = {myStruct.myString}");
+
+            ClassMethod(myClass, myStruct, 3, "Chunita");
+
+            Console.WriteLine($"Dopo myClass.myInteger = {myClass.myInteger}, myClass.myString = {myClass.myString}");
+            Console.WriteLine($"Dopo myStruct.myInteger = {myStruct.myInteger}, myStruct.myString = {myStruct.myString}");
+        }
+        static void ClassMethod(MyClass myClass, MyStruct myStruct, int p_int, string p_string) {
+            myClass.myInteger = p_int;
+            myStruct.myInteger = p_int;
+
+            myClass.myString = p_string;
+            myStruct.myString = p_string;
+        }
+    }
+}
