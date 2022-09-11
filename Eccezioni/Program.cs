f5ddbb1f954981107438c9c98d8a94d5716505af
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-11 14:44:59 +0200

    Adding the finally block

---
+++ Eccezioni/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Eccezioni
+{
+    public class Program {
+        static void Main(string[] args) {
+            int? aNumber = 0;
+
+            Console.Write("Type a number: ");
+            string aNumberStr = Console.ReadLine();
+
+            try {
+                aNumber = Convert.ToInt32(aNumberStr);
+            } catch (System.FormatException formatException) {
+                Console.WriteLine($"You must type a valid number");
+                aNumber = -1;
+            } finally {
+                Console.WriteLine($"You typed {aNumberStr}");
+            }
+        }
+    }
+}
