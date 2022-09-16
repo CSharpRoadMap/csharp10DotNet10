77966936e5920c4019a69263879d3ebbcf61e2e6
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-16 17:34:36 +0200

    Il modificatore in

---
+++ ModificatoreIn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ModificatoreIn
+{
+    class MyClass
+    {
+        public int x = 5;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 5;
+            MyClass mc = new MyClass();
+            Console.WriteLine($"Before calling MyMethod ==> mc.x = {mc.x}, m = {m}");
+
+            MyMethod(mc, m);
+            Console.WriteLine($"After calling MyMethod ==> mc.x = {mc.x}, m = {m}");
+        }
+
+        static void MyMethod(in MyClass y, in int k)
+        {
+            //myClass = new MyClass();    // ERROR!!
+            //k = 10; // ERROR!!
+            y.x = 10;
+        }
+    }
+}
---
+++ ModificatoreOut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ModificatoreOut
+{
+    class MyClass
+    {
+        public int x = 5;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            MyClass myClass;
+            MyMethod(out myClass, out k);
+            Console.WriteLine($"myClass.x = {myClass.x}, k = {k}"); // 10, 10
+        }
+
+        static void MyMethod(out MyClass myClass, out int k)
+        {
+            myClass = new MyClass();
+            myClass.x = 10;
+            k = 10;
+        }
+    }
+}
