f67be0ae0f30fe7548624b095b90e1bb036d2c80
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-17 19:46:15 +0200

    Expression-bodied members

---
+++ ExpressionBodiedMembers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace ExpressionBodiedMembers
+{
+    public class Program
+    {
+        private static string name;
+
+        static string Name {
+            get => name.ToUpper();
+            set => name = value;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greeting("Mario"));
+
+            Name = "Susanna";
+            Console.WriteLine(Name);
+
+        }
+
+        /*
+        static string Greeting(string name)
+        {
+            return $"Hello, {name}";
+        }
+        */
+
+        /*
+            Since the previous method is composed by a single expression, it can
+            be converted in a expression-bodied member
+        */
+        static string Greeting(string name) => $"Hello, {name}";
+    }
+}
