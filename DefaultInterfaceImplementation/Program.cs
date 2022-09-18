057e85019ad1718fee1760a449cafdf7e2431a2d
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-18 11:06:21 +0200

    Default Interface implementation

---
+++ DefaultInterfaceImplementation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefaultInterfaceImplementation
+{
+    public interface IMyInterface {
+        void MyMethod(string value);
+        string MyMethod2(string value) => value.ToUpper();
+    }
+    public class Program : IMyInterface
+    {
+        public void MyMethod(string value) => Console.WriteLine(value);
+        // string MyMethod2(string value) => value.ToLower();
+        static void Main(string[] args)
+        {
+            Program aProgram = new Program();
+            aProgram.MyMethod("Hello");
+            string anString = (aProgram as IMyInterface).MyMethod2("Susanna");
+            Console.WriteLine(anString);
+
+        }
+    }
+}
