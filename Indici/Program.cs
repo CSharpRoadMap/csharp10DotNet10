031baac1aeda87198b8e7b8b1b6acfb692adee63
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-18 15:01:57 +0200

    Gli Indici

---
+++ Indici/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Indici
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = { "primo", "secondo", "terzo", "quarto" };
+
+            // two Index instances
+            Index first = 0;
+            Index last = ^1;    // using the index from end operator, for the last array position
+
+            string anStr = myArray[first];
+            Console.WriteLine(anStr);
+
+            anStr = myArray[last];
+            Console.WriteLine(anStr);
+
+            // reducing the code using the index from end operator directly
+
+            string anotherStr = myArray[^1];
+            Console.WriteLine(anotherStr);
+
+            anotherStr = myArray[^3];
+            Console.WriteLine(anotherStr);
+        }
+    }
+}
