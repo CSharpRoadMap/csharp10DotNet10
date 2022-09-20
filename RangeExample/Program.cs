548dc342f883ab8add70160609d6a7f4500a9dad
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-20 09:30:16 +0200

    Range Example

---
+++ RangeExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RangeExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = { "primo", "secondo", "terzo", "quarto" };
+
+            string[] anStrRange = myArray[..2];
+
+            foreach (string item in anStrRange)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            string[] anotherStrRange = myArray[1..];
+
+            foreach (string item in anotherStrRange)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            string[] anotherStringRange = myArray[1..3];
+
+            foreach (string item in anotherStringRange)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
