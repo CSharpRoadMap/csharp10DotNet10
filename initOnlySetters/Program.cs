925d75c784d1f8ad1beb5d8d528f61a84b89174d
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2022-09-21 15:05:02 +0200

    Top-level statements and init-Only Setters

---
+++ initOnlySetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+var instance = new Program {
+    MyProp = "first"
+};
+
+instance.MyProp = "second";
+instance.Show();
+partial class Program
+{
+    public string MyProp { get; init; }
+
+    public void Show()
+    {
+        Console.WriteLine($"MyProp = {MyProp}");
+    }
+}
---
+++ toplevel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+foreach (string item in args)
+{
+    Console.WriteLine($"Arg: {item}");
+}
+/* namespace toplevel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string item in args)
+            {
+                Console.WriteLine($"Arg: {item}");
+            }
+        }
+    }
+} */
